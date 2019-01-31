54286fc026af264512650c3dfd4a94735939fda5
Tran Van Quyet <vanquyettran@gmail.com>
2019-01-31 11:45:42 +0700

    update logo

--- frontend/views/layouts/svgLogo.php
+++ frontend/views/layouts/svgLogo.php
@@ -1,13 +1,13 @@
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212.06 84.9">
-    <defs>
+    <!--<defs>
         <style>.cls-1 {
-                fill: #231f20;
+                fill: #333;
             }
 
             .cls-2 {
                 fill: #fff;
             }</style>
-    </defs>
+    </defs>-->
     <title>Asset 2</title>
     <g id="Layer_2" data-name="Layer 2">
         <g id="Layer_1-2" data-name="Layer 1">

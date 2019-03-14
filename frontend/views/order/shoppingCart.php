bf8c17ceaae0a1bff244760723ac4577afaac209
vanquyettran <tvq16091993tvq>
2019-03-15 01:22:18 +0700

    add paypal

--- frontend/views/order/shoppingCart.php
+++ frontend/views/order/shoppingCart.php
@@ -82,6 +82,28 @@
                 </div>
             </form>
         </div>
+        <div>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_cart">
+                <input type="hidden" name="business" value="5Y8HZYMY7KUSW">
+                <input type="hidden" name="lc" value="VN">
+                <input type="hidden" name="button_subtype" value="services">
+                <input type="hidden" name="no_note" value="0">
+                <input type="hidden" name="upload" value="1">
+                <input type="hidden" name="cn" value="Add special instructions to the seller:">
+                <input type="hidden" name="no_shipping" value="2">
+                <input type="hidden" name="amount_1" value="120">
+                <input type="hidden" name="amount_2" value="1200">
+                <input type="hidden" name="item_name_1" value="Cart Checkout">
+                <input type="hidden" name="item_number_1" value="3">
+                <input type="hidden" name="item_name_2" value="Cart Checkout 2">
+                <input type="hidden" name="item_number_2" value="2">
+                <input type="hidden" name="currency_code" value="USD">
+                <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+            </form>
+        </div>
     </div>
 </div>
 
@@ -141,6 +163,10 @@
     }
 
     fillUpShoppingCartView();
+
+    document.querySelector('[name="amount"]').value = getCacheData('shoppingCartItems', []).reduce(function (amount, item) {
+        return amount + item.quantity * item.discountedPrice;
+    }, 0);
 </script>
 
 <script>
